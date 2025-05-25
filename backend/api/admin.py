a6ee8ea43dfc2d8bfafc6862119e5eaeb89914fc
KacperMoroch <kacpermoroch@gmail.com>
2025-05-26 00:59:35 +0200

    Registered UserPlayerAssignment and UserGuessLog models in admin panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Role, UserAccount, Country, League, Club, Position, Age, ShirtNumber, Player
 
+
+# rejestrowanie modeli w panelu administracyjnym Django, dzięki czemu można nimi zarządzać w interfejsie admina
 admin.site.register(Role)
 admin.site.register(UserAccount)
 admin.site.register(Country)
