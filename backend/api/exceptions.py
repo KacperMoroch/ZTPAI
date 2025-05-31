51a6b21f901c1a9a1ef624eb1a8d3d8de64d3c23
KacperMoroch <kacpermoroch@gmail.com>
2025-06-01 01:05:00 +0200

    Added new custom API exceptions for improved error handling

--- backend/api/exceptions.py
+++ backend/api/exceptions.py
@@ -49,3 +49,53 @@ class UserNotFoundException(APIException):
     status_code = 404
     default_detail = "Użytkownik nie został znaleziony!"
     default_code = "user_not_found"
+
+class InvalidLoginException(APIException):
+    status_code = 400
+    default_detail = "Login musi mieć co najmniej 4 znaki!"
+    default_code = "invalid_login"
+
+class InvalidEmailException(APIException):
+    status_code = 400
+    default_detail = "Niepoprawny adres e-mail!"
+    default_code = "invalid_email"
+
+class EmailTakenException(APIException):
+    status_code = 400
+    default_detail = "Ten E-mail jest już zajęty."
+    default_code = "email_taken"
+
+class LoginTakenException(APIException):
+    status_code = 400
+    default_detail = "Login jest już zajęty."
+    default_code = "login_taken"
+
+class AccountUpdateFailedException(APIException):
+    status_code = 500
+    default_detail = "Wystąpił błąd podczas aktualizacji konta!"
+    default_code = "account_update_failed"
+
+class MissingFileException(APIException):
+    status_code = 400
+    default_detail = "Brak pliku w żądaniu!"
+    default_code = "missing_file"
+
+class MissingPlayerNameException(APIException):
+    status_code = 400
+    default_detail = "Brak nazwy piłkarza!"
+    default_code = "missing_player_name"
+
+class GameNotStartedException(APIException):
+    status_code = 400
+    default_detail = "Gra nie została jeszcze rozpoczęta."
+    default_code = "game_not_started"
+
+class NoMoreAttemptsException(APIException):
+    status_code = 403
+    default_detail = "Wykorzystano wszystkie próby. Spróbuj ponownie jutro."
+    default_code = "no_more_attempts"
+    
+class AlreadyGuessedException(APIException):
+    status_code = 403
+    default_detail = "Już zgadłeś dzisiaj piłkarza."
+    default_code = "already_guessed"
