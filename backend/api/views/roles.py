f445f8db0aa6380d1de7867c09adf71c99ca0c0d
KacperMoroch <kacpermoroch@gmail.com>
2025-06-01 01:01:53 +0200

    Refactored get_roles endpoint to use RoleSerializer and added Swagger schema for response

--- backend/api/views/roles.py
+++ backend/api/views/roles.py
@@ -5,23 +5,22 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from drf_yasg.utils import swagger_auto_schema
-from drf_yasg import openapi
-
 from api.models import Role
-
-
+from api.serializers import RoleSerializer
+from drf_yasg import openapi
 
 @swagger_auto_schema(
     method='get',
-    operation_description="Pobierz listę dostępnych ról",
-    responses={
-        200: openapi.Response(description="Lista ról")
-    }
+    operation_description="Pobierz listę dostępnych ról. Wynikiem jest lista obiektów zawierających identyfikator oraz nazwę roli.",
+    responses={200: openapi.Response(
+        description="Lista ról",
+        schema=RoleSerializer(many=True)
+    )}
 )
 @authentication_classes([JWTAuthentication])
 @permission_classes([IsAuthenticated])
 @api_view(['GET'])
 def get_roles(request):
     roles = Role.objects.all()
-    role_list = [{"id": role.id, "rola": role.rola} for role in roles]
-    return Response(role_list, status=status.HTTP_200_OK)
+    serializer = RoleSerializer(roles, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
