ede663b586c1ef8f5028da41071e4b9e94a13928
KacperMoroch <kacpermoroch@gmail.com>
2025-06-04 13:19:02 +0200

    Registered additional models in Django admin: UserPlayerAssignment, UserGuessLog, Transfer, TransferQuestionOfTheDay, UserGuessLogTransfer

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from .models import Role, UserAccount, Country, League, Club, Position, Age, ShirtNumber, Player
+from .models import (
+    Role,
+    UserAccount,
+    Country,
+    League,
+    Club,
+    Position,
+    Age,
+    ShirtNumber,
+    Player,
+    UserGuessLog,
+    UserPlayerAssignment,
+    Transfer,
+    TransferQuestionOfTheDay,
+    UserGuessLogTransfer,
+)
 
-
-# rejestrowanie modeli w panelu administracyjnym Django, dzięki czemu można nimi zarządzać w interfejsie admina
+# Rejestrowanie modeli w panelu administracyjnym Django
 admin.site.register(Role)
 admin.site.register(UserAccount)
 admin.site.register(Country)
@@ -12,3 +26,8 @@ admin.site.register(Position)
 admin.site.register(Age)
 admin.site.register(ShirtNumber)
 admin.site.register(Player)
+admin.site.register(UserPlayerAssignment)
+admin.site.register(UserGuessLog)
+admin.site.register(Transfer)
+admin.site.register(TransferQuestionOfTheDay)
+admin.site.register(UserGuessLogTransfer)
