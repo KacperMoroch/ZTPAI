f3d500858d9544f336ba34329935124ef3a514f8
KacperMoroch <kacpermoroch@gmail.com>
2025-06-04 13:22:25 +0200

    Tested artificial delay in registration email task

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -1,9 +1,11 @@
 from celery import shared_task
 from django.core.mail import send_mail
+import time
 
 @shared_task
 def send_registration_email(email):
     print(f"Wysyłam maila do: {email}")
+    # time.sleep(10)
     subject = 'Witamy!'
     message = 'Dziękujemy za rejestrację w naszej aplikacji.'
     from_email = 'goaldle.noreply@gmail.com'
